d546c5c2fdd12c1b0f4fae8cd15a89c88bc27739
Balogun Gbenga <gbalogun4439@stu.ui.edu.ng>
2022-04-08 22:11:53 +0000

    ifelse

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *   main - program that assign a random number to the variable
+ *   Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
