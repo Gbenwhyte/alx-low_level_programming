ef9a2cc329a4d47d0fa58f18ce8aff9efb415166
Balogun Gbenga <gbalogun4439@stu.ui.edu.ng>
2022-04-08 20:45:03 +0000

    ifesle

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main- print if number is positive, zero or ngative
+ * return: Always (success)
+ */
+int main (void)
+{
+	int;
+
+	srand(time(0));
+	n = rand () - RAND_MAX / 2;
+	
+	if (n > 0)
+		printf ("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else 
+		printf("%d is negative\n", n);
+	return (0);
+
+}
