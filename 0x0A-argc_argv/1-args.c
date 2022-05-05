8f5a20d79473b8cc556dc5ecb811f99c12742e4c
Balogun Gbenga <gbalogun4439@stu.ui.edu.ng>
2022-05-05 17:01:53 +0000

    hepl

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
-  * main- Write a program that prints the number of arguments passed into it.
-  * @argc: Count the number of parameters that go into main.
-  * @argv: Pointer of array of pointers ontaining strings entering main.
-  * Return: 0;
-  */
-int main(int argc, char *argv[])
+ *  main - Entry point
+ *  @argc: Counts the number of parameters that go into main
+ *  @argv: Pointer of array of pointers containing strings entering main
+ *  Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
 {
 	(void) argv;
-	printf("%d\n", argc);
+	printf("%i\n", argc - 1);
 	return (0);
 }
