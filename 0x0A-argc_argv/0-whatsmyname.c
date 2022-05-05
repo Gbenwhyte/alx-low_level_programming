f3af09c5729cbf12cf72290c93980094166145d1
Balogun Gbenga <gbalogun4439@stu.ui.edu.ng>
2022-05-05 16:33:25 +0000

    happycommit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main- Write a program that prints its name, followed by a new line.
+  * @argc: count the number of parameters that go into main
+  * @argv: pointer of array of pointers containing strings entering main
+  * return- 0.
+  */
+
+int main(int argc, char **argv)
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	exit(0);
+}
