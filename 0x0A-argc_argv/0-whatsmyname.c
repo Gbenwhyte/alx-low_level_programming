55e7335a25f115507c56cc24b3239d3c14a726bc
Balogun Gbenga <gbalogun4439@stu.ui.edu.ng>
2022-05-05 17:07:32 +0000

    hap

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
-  * main- Write a program that prints its name, followed by a new line.
-  * @argc: count the number of parameters that go into main
-  * @argv: pointer of array of pointers containing strings entering main
-  * return- 0.
-  */
+ *  main - Entry point
+ *  @argc: Counts the number of parameters that go into main
+ *  @argv: Pointer of array of pointers containing strings entering main
+ *  Return: Always 0 (Success)
+ */
 
 int main(int argc, char **argv)
 {
